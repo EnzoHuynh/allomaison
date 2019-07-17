6b0f410dd19b501a4de605e03361747c0efbc2d7
Amandine <amandine-ripoll@outlook.fr>
2019-07-17 10:59:17 +0200

    add images

--- server/app.js
+++ server/app.js
@@ -1,16 +1,16 @@
 const app = require('express')();
 const verifyToken = require('./middlewares/security');
-const movieRouter = require('./routes/movie');
 const userRouter = require('./routes/user');
 const securityRouter = require('./routes/security');
 const goodRouter = require('./routes/good');
+const imageRouter = require('./routes/image');
 const bodyparser = require('body-parser');
 
 app.use(bodyparser.json());
 app.use(verifyToken);
 app.use('/', securityRouter);
-app.use('/movies', movieRouter);
 app.use('/users', userRouter);
 app.use('/goods', goodRouter);
+app.use('/images', imageRouter);
 
 app.listen(3000, () => console.log('Listening on port 3000'));
