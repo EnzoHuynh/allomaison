4fdb1571c776d0c9711dd8a5c41daa6850467a23
Enzo Huynh <enzo_huynh@hotmail.com>
2019-07-21 23:01:49 +0200

    Fix error profile

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,44 +1,29 @@
 import React, { useState, useEffect } from 'react';
 
-import goodApi from '../utils/goodApi';
+// import goodApi from '../utils/goodApi';
 
 const Profile = () => {
   const user = JSON.parse(localStorage.getItem('user'));
-  const { goods, setGoods } = useState(null);
+  // const { goods, setGoods } = useState(null);
 
-  const fetchGoods = () => {
-    goodApi.getGoods(user.id).then(setGoods(goods));
-  };
+  // const fetchGoods = () => {
+  //   goodApi.getGoods(user.id).then(setGoods(goods));
+  // };
 
-  console.log(fetchGoods);
-  useEffect(() => {
-    fetchGoods();
-  }, [user.id]);
+  // console.log(fetchGoods);
+  // useEffect(() => {
+  //   fetchGoods();
+  // }, [user.id]);
 
   return (
     <section className="section">
       <div className="container">
         <div className="columns">
           <div className="column is-6 is-offset-3">
-<<<<<<< HEAD
-            <div className="box">
-              <figure className="image is-128x128" style={{ margin: '0 auto' }}>
-                <img
-                  className="is-rounded"
-                  src={
-                    user && 'path_img' in user
-                      ? user.path_img
-                      : 'https://randomuser.me/api/portraits/women/79.jpg'
-                  }
-                />
-              </figure>
-            </div>
-=======
             <h1 className="title is-5 has-text-centered">Profil</h1>
             <label>Email : {user.email}</label>
->>>>>>> 1d3aa163c49d6dee72f9d665344a41b4ecd4be46
           </div>
-          {goods &&
+          {/* {goods &&
             goods.map(goods => (
               <>
                 <div>{goods.title}</div>
@@ -51,7 +36,7 @@ const Profile = () => {
                 <div>{goods.bedroom}</div>
                 <div>{goods.square_meters}</div>
               </>
-            ))}
+            ))} */}
         </div>
       </div>
     </section>
