6d4293a7e49b79b698cc9654e2f9a5d2a42f5249
taingk <taing_k@etna-alternance.net>
2019-07-24 00:47:36 +0200

    add goods test

---
+++ client/src/pages/__tests__/goods.js
@@ -0,0 +1,20 @@
+import '@testing-library/react/cleanup-after-each';
+import '@testing-library/jest-dom/extend-expect';
+// NOTE: jest-dom adds handy assertions to Jest and is recommended, but not required
+
+import React from 'react';
+import { render, waitForElement } from '@testing-library/react';
+import Goods from '../goods';
+
+const match = {
+  params: {
+    terms: 'Massy',
+  },
+  path: '/louer/Massy',
+};
+
+test('Goods are fetched', async () => {
+  const { getAllByText } = render(<Goods match={match} />);
+
+  await waitForElement(() => getAllByText(/Massy/i));
+});
