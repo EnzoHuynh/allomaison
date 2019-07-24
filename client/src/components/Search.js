47dfc3acaef8c422541b4baf7fe173fe452f9ecc
taingk <taing_k@etna-alternance.net>
2019-07-24 12:14:17 +0200

    search ok

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -20,36 +20,49 @@ const Search = ({ history }) => {
   };
 
   return (
-    <form>
-      <select value={type} onChange={e => setType(e.target.value)}>
-        <option value="acheter">Acheter</option>
-        <option value="louer">Louer</option>
-      </select>
-      <label htmlFor="search">Recherche</label>
-      <Autocomplete
-        inputProps={{ id: 'search' }}
-        getItemValue={item => item.nom}
-        items={items}
-        renderItem={(item, isHighlighted) => (
-          <div
-            key={item.code}
-            style={{ background: isHighlighted ? 'lightgray' : 'white' }}
-          >
-            {item.nom}
+    <section class="hero is-link is-fullheight-with-navbar">
+      <div class="hero-body">
+        <form className="container">
+          <div>
+            <label className="title" htmlFor="search">
+              Recherche de bien
+            </label>
           </div>
-        )}
-        value={terms}
-        onChange={onSearch}
-        onSelect={val => setTerms(val)}
-        autoHighlight
-      />
-      <button onClick={onClick}>Rechercher</button>
-      <div>
-        {items.map(item => (
-          <p key={item.code}>{item.nom}</p>
-        ))}
+          <br />
+          <div className="select">
+            <select value={type} onChange={e => setType(e.target.value)}>
+              <option value="acheter">Acheter</option>
+              <option value="louer">Louer</option>
+            </select>
+          </div>
+          <Autocomplete
+            inputProps={{ id: 'search', className: 'input' }}
+            getItemValue={item => item.nom}
+            items={items}
+            renderItem={(item, isHighlighted) => (
+              <div
+                key={item.code}
+                style={{ background: isHighlighted ? 'lightgray' : 'black' }}
+              >
+                {item.nom}
+              </div>
+            )}
+            value={terms}
+            onChange={onSearch}
+            onSelect={val => setTerms(val)}
+            autoHighlight
+          />
+          <button className="button" onClick={onClick}>
+            Rechercher
+          </button>
+          <div hidden>
+            {items.map(item => (
+              <p key={item.code}>{item.nom}</p>
+            ))}
+          </div>
+        </form>
       </div>
-    </form>
+    </section>
   );
 };
 
